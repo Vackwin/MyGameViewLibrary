48cb43c9f37c084ea89b956db3edbe3f47a467fa
Brian <ad715310@gmail.com>
2021-06-30 15:41:39 +0800

    add Imageview and other funs

--- MyGameLibrary/src/main/java/com/example/android/mygamelibrary/MyGameView.kt
+++ MyGameLibrary/src/main/java/com/example/android/mygamelibrary/MyGameView.kt
@@ -4,28 +4,43 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 class MyGameView : ConstraintLayout {
-    interface MyCallback{
+    interface MyCallback {
         fun onPrizeSelected(prize: Prize, drawTimes: Int)
     }
-    private var callback : MyCallback? = null
+
+    private var callback: MyCallback? = null
     private var prizes = mutableListOf<Prize>()
-    private var imgBtn1 : ImageButton
-    private var imgBtn2 : ImageButton
-    private var imgBtn3 : ImageButton
-    private var imgBtn4 : ImageButton
+    private var tv: TextView
+    private var imgBtn1: ImageButton
+    private var imgBtn2: ImageButton
+    private var imgBtn3: ImageButton
+    private var imgBtn4: ImageButton
+    private var prizeImage: ImageView
     private var drawTimes = 0
-    constructor(context: Context):this(context,null)
-    constructor(context: Context, attrs: AttributeSet?):this(context, attrs,0)
+    private lateinit var imgButtons: List<ImageButton>
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
         View.inflate(context, R.layout.mygameview, this)
+        tv = findViewById(R.id.game_title)
         imgBtn1 = findViewById(R.id.img1)
         imgBtn2 = findViewById(R.id.img2)
         imgBtn3 = findViewById(R.id.img3)
         imgBtn4 = findViewById(R.id.img4)
+        prizeImage = findViewById(R.id.prize_image)
+        imgButtons = listOf(
+                imgBtn1,
+                imgBtn2,
+                imgBtn3,
+                imgBtn4
+        )
         setListeners()
         if (attrs != null) {
             context.theme.obtainStyledAttributes(
@@ -35,39 +50,54 @@ class MyGameView : ConstraintLayout {
             )
         }
     }
-    fun setCallback(cb : MyCallback){
+
+    fun setCallback(cb: MyCallback) {
         callback = cb
     }
+
     private fun setListeners() {
-        val imgButtons : List<ImageButton> = listOf(
-                imgBtn1,
-                imgBtn2,
-                imgBtn3,
-                imgBtn4
-        )
-        for(item in imgButtons){
-            item.setOnClickListener{
-                    drawTimes++
-                    val prize = drawPrize()
-                    callback?.onPrizeSelected(prize, drawTimes)
+        for (item in imgButtons) {
+            item.setOnClickListener {
+                drawTimes++
+                val prize = drawPrize()
+                prizeImage.setImageBitmap(prize.img)
+                tv.visibility = View.INVISIBLE
+                for (item in imgButtons) {
+                    item.visibility = View.INVISIBLE
+                }
+                prizeImage.visibility = View.VISIBLE
+                callback?.onPrizeSelected(prize, drawTimes)
             }
         }
     }
-    fun setPrizes(prizes : List<Prize>){
+
+    fun setPrizes(prizes: List<Prize>) {
         this.prizes.clear()
         this.prizes.addAll(prizes)
     }
-    private fun drawPrize() : Prize {
+
+    private fun drawPrize(): Prize {
         val randomProb: MutableList<Int> = mutableListOf()
-        for(item in prizes){
-            val x = Random().nextInt((item.probability*10000).toInt())
+        for (item in prizes) {
+            val x = Random().nextInt((item.probability * 10000).toInt())
             randomProb.add(x)
         }
-        if(prizes.isEmpty()){
+        if (prizes.isEmpty()) {
             throw(Exception("Need to setPrizes() first."))
         }
         return prizes[randomProb.indexOf(randomProb.maxOrNull())]
 
+    }
+
+    fun hidePrize(): Unit {
+        this.prizeImage.visibility = View.INVISIBLE
+    }
+
+    fun showUi(): Unit {
+        tv.visibility = View.VISIBLE
+        for (item in imgButtons) {
+            item.visibility = View.INVISIBLE
+        }
     }
 
 }
--- MyGameLibrary/src/main/java/com/example/android/mygamelibrary/Prize.kt
+++ MyGameLibrary/src/main/java/com/example/android/mygamelibrary/Prize.kt
@@ -1,4 +1,4 @@
 package com.example.android.mygamelibrary
 import android.graphics.Bitmap
 
-class Prize (val probability : Float, val img : Bitmap)
+class Prize (val probability : Float, val img : Bitmap, val name : String, val description : String)
